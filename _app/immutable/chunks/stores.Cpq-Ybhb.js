43f3ccc2290147eb41ed63c3d3cca35824749f8e
Arun Kumar <arunkakorp@gmail.com>
2024-09-20 09:57:05 +0530

    Updates

---
+++ _app/immutable/chunks/stores.Cpq-Ybhb.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index.C5X9v00r.js";const a=e({location:"init",health:50,energy:{social:100,wierd:100},neuro:{},gender:{},sexuality:{},alertLevel:40});export{a as g};
---
+++ _app/immutable/entry/start.DNGsfWca.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DN_1RrEs.js";export{t as start};
