560e35f553affef18c7e4f392e49a3e1a9b33997
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-09-30 00:21:36 +0530

    (/usr/share/games/fortunes/art) % Just because you like my stuff doesn't mean I owe you anything. 		-- Bob Dylan

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -75,90 +75,99 @@
                                           };
                 $gameState.user.alertLevel = clampValue(result.alertness);
                 }, $gameParams.TICK);
-            }
+    }
+    function initializeCity(canvas) {
+          city = canvas.group();
+          city.svg(citySvg);
+          city.move(0, 0);
+          city.size(800, 600);
+          canvas.add(city);
+          for (const loc in $gameParams.locations) {
+              const location = $gameParams.locations[loc];
+              const svg = buildingIconMap[location.icon];
+              const group = canvas.group();
+              group.svg(svg);
+              if ('dimensions' in location) {
+                  group.size(location.dimensions.width, location.dimensions.height);
+              } else {
+                  group.size($gameParams.defaults.buildingDimensions.width, $gameParams.defaults.buildingDimensions.height);
+              }
+              const label = canvas.text(function(add) {
+                  add.tspan(location.label).fill('#fff');
+              });
 
-    function initializeGameState(canvas) {
-        const style = canvas.style('.mycolor', { color: 'pink' });
-        canvas.add(style);
-        city = canvas.group();
-        city.svg(citySvg);
-        city.move(0, 0);
-        city.size(800, 600);
-        canvas.add(city);
-        for (const loc in $gameParams.locations) {
-            const location = $gameParams.locations[loc];
-            const svg = buildingIconMap[location.icon];
-            const group = canvas.group();
-            group.svg(svg);
-            if ('dimensions' in location) {
-                group.size(location.dimensions.width, location.dimensions.height);
-            } else {
-                group.size($gameParams.defaults.buildingDimensions.width, $gameParams.defaults.buildingDimensions.height);
-            }
-            const label = canvas.text(function(add) {
-                add.tspan(location.label).fill('#fff');
-            });
-        let x = 0;
-        let y = 0;
-        if ($gameState.state == 'mapcreated') {
-            console.log('Map has been created and saved:');
-            [x, y] = $gameState.map[loc];
-            console.log(`${loc}: ${x}, ${y}`);
-        } else {
-            [[x, y]] = buildingPositions.splice(Math.floor(Math.random()*buildingPositions.length), 1);
-        }
-        group.move(x, y)
-        label.move(x, y-20);
-        group.add(label);
-        //Tailwind helper attributes to trigger the modal Menu boxes
-        group.data('data-modal-target', `id-${loc}`);
-        group.data('data-modal-toggle', `id-${loc}`);
-        group.click(() => modalShows[loc] = true);
-        group.css('cursor', 'pointer');
-        canvas.add(group);
-        $gameState.map[loc] = [x,y];
-        if (location.label == 'Home' && $gameState.state == 'ready') {
-            house = group;
+              let x = 0;
+              let y = 0;
+              if ($gameState.state == 'mapcreated') {
+                  console.log('Map has been created and saved:');
+                  [x, y] = $gameState.map[loc];
+                  console.log(`${loc}: ${x}, ${y}`);
+              } else {
+                  [[x, y]] = buildingPositions.splice(Math.floor(Math.random()*buildingPositions.length), 1);
+              }
+              group.move(x, y)
+              label.move(x, y-20);
+              group.add(label);
+              //Tailwind helper attributes to trigger the modal Menu boxes
+              group.data('data-modal-target', `id-${loc}`);
+              group.data('data-modal-toggle', `id-${loc}`);
+              group.click(() => modalShows[loc] = true);
+              group.css('cursor', 'pointer');
+              canvas.add(group);
+              $gameState.map[loc] = [x,y];
 
-            player = canvas.group();
-            player.svg(personSvg);
-            player.size($gameParams.defaults.player.dimensions.width, $gameParams.defaults.player.dimensions.height);
+              if (location.label == 'Home' && $gameState.state == 'ready') {
+                  house = group;
 
-            const playerLabel = canvas.text(function(add) {
-                add.tspan("Player").fill('#fff').addClass('mycolor').css('cursor', 'pointer');
-            });
-            player.add(playerLabel);
-            //30 to the right of the Home
-            console.log(`x: ${x}, y: ${y}`);
-            const pos = player.point(x,y);
-            console.log(pos);
-            player.move(x+30, y);
-            playerLabel.move(x+30, y-10)
-            canvas.add(player);
-            $gameState.map['player'] = [x+30, y];
-        }
-        if ($gameState.state == 'ready') {
-            $gameState.state = 'mapcreated';
-        } else {
-            const player = canvas.group();
-            player.svg(personSvg);
-            player.size($gameParams.defaults.player.dimensions.width, $gameParams.defaults.player.dimensions.height);
-            const playerLabel = canvas.text(function(add) {
-                add.tspan("Player").fill("#fff").css('cursor', 'pointer');
-            });
-            player.add(playerLabel);
-            const [x, y] = $gameState.map['player'];
-            player.move(x, y);
-            playerLabel.move(x, y-10);
-            canvas.add(player);
+                  player = canvas.group();
+                  player.svg(personSvg);
+                  player.size($gameParams.defaults.player.dimensions.width, $gameParams.defaults.player.dimensions.height);
+
+                  const playerLabel = canvas.text(function(add) {
+                      add.tspan("Player").fill('#fff').addClass('mycolor').css('cursor', 'pointer');
+                  });
+                  player.add(playerLabel);
+                  //30 to the right of the Home
+                  console.log(`x: ${x}, y: ${y}`);
+                  const pos = player.point(x,y);
+                  console.log(pos);
+                  player.move(x+30, y);
+                  playerLabel.move(x+30, y-10)
+                  canvas.add(player);
+                  $gameState.map['player'] = [x+30, y];
+              }
+
+          if ($gameState.state == 'ready') {
+              $gameState.state = 'mapcreated';
+          } else {
+              const player = canvas.group();
+              player.svg(personSvg);
+              player.size($gameParams.defaults.player.dimensions.width, $gameParams.defaults.player.dimensions.height);
+              const playerLabel = canvas.text(function(add) {
+                  add.tspan("Player").fill("#fff").css('cursor', 'pointer');
+              });
+              player.add(playerLabel);
+              const [x, y] = $gameState.map['player'];
+              player.move(x, y);
+              playerLabel.move(x, y-10);
+              canvas.add(player);
+          }
         }
     }
+
+    function initializeGameCanvas(canvas) {
+        const style = canvas.style('.mycolor', { color: 'pink' });
+        canvas.add(style);
+        let randMap = engine.gm();
+        for (let i=0; i++; i < randMap.city.length){
+          initializeCity(canvas);
+        }
     // add bot players
     let bots = engine.gb($gameParams.locations);
     //$gameState.locationUserMap = bots.locationUserMap;
-    bots.locationUserMap.forEach((item) => {
-        $gameState.locationUserMap.push(item);
-    });
+    //bots.locationUserMap.forEach((item) => {
+    //    $gameState.locationUserMap.push(item);
+    //});
     $gameState.allUsers = bots.allUsers;
 
     loading.set(false);
@@ -168,18 +177,18 @@
         //const flowbite = await import('flowbite');
         initFlowbite();
         canvas = SVG().addTo('#canvas').size($gameParams.board.width, $gameParams.board.height);
-        initializeGameState(canvas);
-        updatePlayerStats
+        initializeGameCanvas(canvas);
+        //updatePlayerStats();
     });
 
     gameState.subscribe((value) => {
         console.log('user health changed. new value:');
-        console.log($gameState.user.energy);
-        console.log($gameState.user.Location);
-        console.log($gameState.locationUserMap);
-        if (value.user.health <= 0 || value.user.energy <= 0) {
-          goto('/deadpage');
-        }
+
+        //\let tot_energy = mean(value.user.energy);
+        //console.log(tot_energy);
+        //if (value.user.health <= 0 || tot_energy <= 0) {
+        //  goto('/deadpage');
+        //}
     });
     function parseDurationToMs(duration) {
         const hours = parseFloat(duration.split(" ")[0]);
@@ -201,27 +210,9 @@
         player.move(x+30, y);
         playerLabel.move(x+30, y+10);
         $gameState.map['player'] = [x+30, y];
-
-        // Now calculate the stats.
-        // $gameState.time += parseDurationToMs(choice.duration);
-        // $gameState.user.health = clampValue($gameState.user.health + choice.effect.health);
-        // $gameState.user.alertness = clampValue($gameState.user.alertness + choice.effect.alertness);
-        // console.log(typeof choice.effect.energy);
-        // if (typeof choice.effect.energy == 'object') {
-        //     $gameState.user.energy.social = clampValue($gameState.user.energy.social + choice.effect.energy.social);
-        //     $gameState.user.energy.weird = clampValue($gameState.user.energy.weird + choice.effect.energy.weird);
-        //     $gameState.user.energy.restless = clampValue($gameState.user.energy.restless + choice.effect.energy.restless);
-        // } else {
-        //     $gameState.user.energy.social = clampValue($gameState.user.energy.social + choice.effect.energy);
-        //     $gameState.user.energy.weird = clampValue($gameState.user.energy.weird + choice.effect.energy);
-        //     $gameState.user.energy.restless = clampValue($gameState.user.energy.restless + choice.effect.energy);
-        // }
     }
 
     const handleMouseDown = event => {
-        /* console.log(event); */
-        /* console.log(canvas.node); */
-        /* console.log(house.node); */
         if (event.target == canvas.node) {
             //player.move(event.pageX-450, event.pageY-50);
             $gameState.user.energy.social -= 10;
